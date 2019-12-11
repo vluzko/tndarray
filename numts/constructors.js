9149615d3ad33d3d5cc77986f17effa0e9f81567
Vincent Luczkow <vincent.luczkow@gmail.com>
2019-12-11 12:47:10 -0500

    Removed js files

--- numts/constructors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function eye(n) {
-    const shape = new Uint32Array([n, n]);
-    throw new Error();
-}
-exports.eye = eye;
-//# sourceMappingURL=constructors.js.map
