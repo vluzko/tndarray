c814cd6fbec663ca2bd4828b75d887f5dbf09ca5
Vincent Luczkow <vincent.luczkow@gmail.com>
2020-02-22 19:42:20 -0500

    A bit of the API for bayes nets

--- numts/ml/pgm.ts
+++ numts/ml/pgm.ts
@@ -1,3 +1,37 @@
 /** Probabilistic graphical models */
 
+import {tensor} from '../tensor';
 
+/**
+ * A simple DAG implementation
+ */
+class DAG {
+    public nodes: Uint32Array;
+    public adjacency_list: Map<number, [number[], number[]]>;
+
+    constructor(nodes: Uint32Array, adjacency_list: Map<number, [number[], number[]]) {
+        this.nodes = nodes;
+        this.adjacency_list = adjacency_list;
+    }
+}
+
+
+export class BayesNet {
+    public dag: DAG;
+
+    /**
+     * Find the conditional probability distributions that best fit
+     * @param data - The data to fit.
+     */
+    fit(data: tensor) {
+
+    }
+
+    /**
+     * Predict the values of the unknown nodes
+     * @param input - The known nodes
+     */
+    predict(input: Map<number, number>) {
+
+    }
+}
